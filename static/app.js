625b3e6592933ad35a00b0d20ed123162e831e7c
B H <83563712+hbinyomin@users.noreply.github.com>
2026-02-12 15:44:15 -0500

    Add time and date formatting using time zone

--- static/app.js
+++ static/app.js
@@ -2,60 +2,81 @@
 const zmanimDisplay = document.getElementById("zmanimDisplay");
 const locationTitle = document.getElementById("locationTitle");
 const errorDisplay = document.getElementById("errorDisplay");
-const datePicker = document.getElementById('date')
+const datePicker = document.getElementById("date");
 
 setDatePicker();
 
-function setDatePicker() {
-    const today = new Date();
-    datePicker.value = today.toISOString().split('T')[0];
-}
-
-
 async function fetchZmanim() {
-  const location_identifier = document.getElementById("location_identifier").value;
-  const date = document.getElementById("date").value;
-
-  try {
+    const location_identifier = document.getElementById("location_identifier").value.trim();
+    const date = document.getElementById("date").value;
+    let errorData;
     const res = await fetch(`/api/zmanim/?location_identifier=${encodeURIComponent(location_identifier)}&date=${date}`);
     if (!res.ok) {
-      const errorData = await res.json();
-      displayErrorMessage(errorData);
-    } else {
-      const data = await res.json();
-      updateZmanim(data);
+        try {
+            errorData = await res.json();
+        } catch (e) {
+            errorData = { detail: "Error processing error message from the server" };
+        }
+        displayErrorMessage(errorData);
     }
-  }
-  catch (e) {
-    displayErrorMessage({
-      detail: `Sorry, there was an error fetching the data. \n Please contact the site administrator. \n ${e}`,
-      dont_try_again: "True",
-    });
-  }
+    else {
+        const data = await res.json();
+        updateZmanim(data);
+    }
 }
 
 function updateZmanim(data) {
-  clearData();
-  const date = datePicker.value;
-  locationTitle.textContent = `Zmanim for: ${data["location"]["title"]}, for ${date}`;
+    clearData();
+    const formattedDate = getFormattedDate(datePicker.value);
+    locationTitle.textContent = `Zmanim for: ${data["location"]["title"]}, for ${formattedDate}`;
 
-  for (let zman in data.times) {
-    const zmanIsoString = data["times"][zman];
-    const timeDisplayString = new Date(zmanIsoString).toLocaleTimeString();
-    const p = document.createElement("p");
-    p.textContent = `${zman} : ${timeDisplayString}`;
-    zmanimDisplay.append(p);
-  }
+    for (let zman in data.times) {
+        const timeZoneId = data["location"]["tzid"];
+        const formattedTime = getFormattedTime(data["times"][zman], timeZoneId);
+
+        const p = document.createElement("p");
+        p.textContent = `${zman} : ${formattedTime}`;
+        zmanimDisplay.append(p);
+    }
 }
 
+// helper functions
+
 function displayErrorMessage(errorData) {
-  clearData();
-  errorDisplay.innerText = `${errorData.detail} ${errorData.dont_try_again? '': '\n Please try again.'} `;
+    clearData();
+    errorDisplay.innerText = errorData.detail;
+}
+
+function getFormattedTime(timeString, timeZoneId) {
+    const formattedTime = Intl.DateTimeFormat("en-US", {
+        timeZone: timeZoneId,
+        hour: "numeric",
+        minute: "2-digit",
+        second: "2-digit",
+    }).format(new Date(timeString));
+
+    return formattedTime;
+}
+
+function getFormattedDate(dateString) {
+    const dateObj = new Date(dateString);
+    const formattedDate = new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    }).format(dateObj);
+
+    return formattedDate;
+}
+
+function setDatePicker() {
+    const today = new Date();
+    datePicker.value = today.toISOString().split("T")[0];
 }
 
 function clearData() {
-  locationTitle.innerText = "";
-  zmanimDisplay.innerText = "";
-  errorDisplay.innerText = "";
+    locationTitle.innerText = "";
+    zmanimDisplay.innerText = "";
+    errorDisplay.innerText = "";
 }
 // })();
