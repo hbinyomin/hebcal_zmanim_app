306f7a5e95a5adee56d9bba0f630393de0ba69bd
B H <83563712+hbinyomin@users.noreply.github.com>
2026-02-12 16:28:52 -0500

    Human friendly labels

--- static/app.js
+++ static/app.js
@@ -18,8 +18,7 @@ async function fetchZmanim() {
             errorData = { detail: "Error processing error message from the server" };
         }
         displayErrorMessage(errorData);
-    }
-    else {
+    } else {
         const data = await res.json();
         updateZmanim(data);
     }
@@ -31,16 +30,46 @@ function updateZmanim(data) {
     locationTitle.textContent = `Zmanim for: ${data["location"]["title"]}, for ${formattedDate}`;
 
     for (let zman in data.times) {
-        const timeZoneId = data["location"]["tzid"];
-        const formattedTime = getFormattedTime(data["times"][zman], timeZoneId);
+        // only include the specified zmanim
+        if (ZMANIM_LABELS[zman]) {
+            const zmanLabel = ZMANIM_LABELS[zman];
+            const timeZoneId = data["location"]["tzid"];
+            const formattedTime = getFormattedTime(data["times"][zman], timeZoneId);
 
-        const p = document.createElement("p");
-        p.textContent = `${zman} : ${formattedTime}`;
-        zmanimDisplay.append(p);
+            const p = document.createElement("p");
+            p.textContent = `${zmanLabel} : ${formattedTime}`;
+            zmanimDisplay.append(p);
+        }
     }
 }
 
 // helper functions
+const ZMANIM_LABELS = {
+    chatzotNight: "Chatzos Layla",
+    alotHaShachar: "Alos HaShachar 16.1°",
+    misheyakir: "Misheyakir 11.5°",
+    misheyakirMachmir: "Misheyakir 10.2°",
+    sunrise: "Sunrise",
+    sofZmanShmaMGA19Point8: "Sof Zman Krias Shema (Magen Avraham 19.8°)",
+    sofZmanShmaMGA16Point1: "Sof Zman Krias Shema (Magen Avraham 16.1°)",
+    sofZmanShmaMGA: "Sof Zman Krias Shema (Magen Avraham)",
+    sofZmanShma: "Sof Zman Krias Shema (Gra)",
+    sofZmanTfillaMGA19Point8: "Sof Zman Tefilla (Magen Avraham 19.8°)",
+    sofZmanTfillaMGA16Point1: "Sof Zman Tefilla (Magen Avraham 16.1°)",
+    sofZmanTfillaMGA: "Sof Zman Tefilla (Magen Avraham)",
+    sofZmanTfilla: "Sof Zman Tefilla (Gra)",
+    chatzot: "Chatzos",
+    minchaGedola: "Mincha Gedola (Gra)",
+    minchaGedolaMGA: "Mincha Gedola (Magen Avraham)",
+    minchaKetana: "Mincha Ketana (Gra)",
+    minchaKetanaMGA: "Mincha Ketana (Magen Avraham)",
+    plagHaMincha: "Plag HaMincha",
+    sunset: "Sunset",
+    tzeit85deg: "Tzais HaKochavim (8.5°)",
+    tzeit42min: "Tzais HaKochavim (42 min after sunset)",
+    tzeit50min: "Tzais HaKochavim (50 min after sunset)",
+    tzeit72min: "Tzais HaKochavim (72 min after sunset)",
+};
 
 function displayErrorMessage(errorData) {
     clearData();
