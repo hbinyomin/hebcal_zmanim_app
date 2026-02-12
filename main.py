4080b64fb628961f3d07403259d634a48fb902a4
B H <83563712+hbinyomin@users.noreply.github.com>
2026-02-12 01:18:27 -0500

    Uri encoding, cleanup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import requests
+from datetime import date
 
 app = FastAPI()
 
@@ -14,14 +15,9 @@ def home():
     return FileResponse('index.html')
 
 @app.get('/api/zmanim')
-def get_zmanim(location_identifier, date="2026-02-11"):
+def get_zmanim(location_identifier, date : date):
     # For now, the only way to use this endpoint is with a zip code.
     # Further development would allow user to enter a place name, and the endpoint would fetch a lookup from geoNames
-
-    # Hebcal seems to truncate beyond 5 digits, so will not error withouth this line of code
-    if (len(location_identifier)!=5):
-        raise HTTPException(status_code=400, detail="Zip code must be exaclty 5 numbers long.")
-    
     params = {"cfg":"json","sec":1,"zip":location_identifier,"date":date}
     try:
         res = requests.get(HEBCAL_URL,params=params)
@@ -29,7 +25,11 @@ def get_zmanim(location_identifier, date="2026-02-11"):
     except requests.RequestException:
         raise HTTPException(status_code=502, detail="Error fetching Hebcal data")
     
+    # Hebcal seems to truncate beyond 5 digits, so will not error withouth this line of code
+    if (len(location_identifier)!=5 and "error" not in data):
+        raise HTTPException(status_code=400, detail="Zip code must be exaclty 5 numbers long.")
+   
     if "error" in data:
         raise HTTPException(status_code=400, detail=data["error"])
-
+    
     return data
--- static/app.js
+++ static/app.js
@@ -2,32 +2,50 @@
 const zmanimDisplay = document.getElementById("zmanimDisplay");
 const locationTitle = document.getElementById("locationTitle");
 const errorDisplay = document.getElementById("errorDisplay");
+const datePicker = document.getElementById('date')
+
 const BASE_URL = "https://zmanimapp-bjc9b7e3dcd9epgy.mexicocentral-01.azurewebsites.net/";
+
+
+setDatePicker();
+
+function setDatePicker() {
+    const today = new Date();
+    datePicker.value = today.toISOString().split('T')[0];
+}
+
+
 async function fetchZmanim() {
   const location_identifier = document.getElementById("location_identifier").value;
   const date = document.getElementById("date").value;
 
-
-  const res = await fetch(`${BASE_URL}/api/zmanim/?location_identifier=${location_identifier}&date=${date}`);
-  if (!res.ok) {
-    const errorData = await res.json();
-    displayErrorMessage(errorData);
-    console.log("the error data is:", errorData);
-  } else {
-    const data = await res.json();
-    updateZmanim(data);
-    console.log(data);
+  try {
+    const res = await fetch(`${BASE_URL}/api/zmanim/?location_identifier=${encodeURIComponent(location_identifier)}&date=${date}`);
+    // const res = await fetch(`/api/zmanim/?location_identifier=${encodeURIComponent(location_identifier)}&date=${date}`);
+    if (!res.ok) {
+      const errorData = await res.json();
+      displayErrorMessage(errorData);
+    } else {
+      const data = await res.json();
+      updateZmanim(data);
+    }
+  }
+  catch (e) {
+    displayErrorMessage({
+      detail: `Sorry, there was an error fetching the data. \n Please contact the site administrator. \n ${e}`,
+      dont_try_again: "True",
+    });
   }
 }
 
 function updateZmanim(data) {
   clearData();
-  const date = new Date(data["date"]);
-  locationTitle.textContent = `Zmanim for: ${data["location"]["title"]}, for ${date.toLocaleDateString()}`;
+  const date = datePicker.value;
+  locationTitle.textContent = `Zmanim for: ${data["location"]["title"]}, for ${date}`;
 
   for (let zman in data.times) {
-    const isoString = data["times"][zman];
-    const timeDisplayString = new Date(isoString).toLocaleTimeString();
+    const zmanIsoString = data["times"][zman];
+    const timeDisplayString = new Date(zmanIsoString).toLocaleTimeString();
     const p = document.createElement("p");
     p.textContent = `${zman} : ${timeDisplayString}`;
     zmanimDisplay.append(p);
@@ -36,7 +54,7 @@ function updateZmanim(data) {
 
 function displayErrorMessage(errorData) {
   clearData();
-  errorDisplay.innerText = `${errorData.detail} \n Please try again.`;
+  errorDisplay.innerText = `${errorData.detail} ${errorData.dont_try_again? '': '\n Please try again.'} `;
 }
 
 function clearData() {
